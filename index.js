b16dcaa2638f95cec3625c6f0a2fffb353e08eb8
Peter O'Shaughnessy <p_os@yahoo.co.uk>
2013-03-16 12:16:48 +0000

    Check in rubbishy leap code with hand position stuff

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 #!/usr/bin/env node
 
 var arDrone = require('ar-drone');
+var Gesture = require('leapjs').Gesture;
 var Leap = require('leapjs').Leap;
+var Motion = require('leapjs').Motion;
 
 var client = arDrone.createClient();
 
 client.takeoff();
 
+/*
 client
   .after(5000, function() {
     this.clockwise(0.5);
@@ -18,15 +21,92 @@ client
     this.stop();
     this.land();
   });
+*/
 
 Leap.loop({enableGestures: true}, function(frame) {
 
     //console.log(frame);
 
-    if( frame.gestures && frame.gestures.length > 0 ) {
 
-        console.log('WOO GESTURES!', frame.gestures );
+    var hands = frame.hands;
+
+    if( hands.length > 0 ) {
+
+        var firstHand = hands[0];
+
+        ///console.log(firstHand);
+
+        var position = firstHand.palmPosition;
+
+        var yPos = position[1];
+
+        console.log(yPos);
+
+        var adjustedYPos = (yPos - 60) / 50;
+
+        console.log('adjustedYPos', adjustedYPos);
+
+        /*
+        // Invert
+        var zPosition = -position[2];
+
+        var adjustedZPosition = (zPosition - 20) / 10;
+
+        console.log('adjustedZPosition', adjustedZPosition);
+        */
 
     }
+
+
+    /*
+    var fingers = frame.fingers;
+
+    if( fingers.length > 0 ) {
+
+        var firstFinger = fingers[0];
+
+        var position = firstFinger.tipPosition;
+
+        // Invert
+        var zPosition = -position[2];
+
+        var adjustedZPosition = (zPosition - 20) / 10;
+
+        console.log('adjustedZPosition', adjustedZPosition);
+
+    }
+    */
+
+    /*
+    var gestures = frame.gestures;
+
+    if( gestures && gestures.length > 0 ) {
+
+        for( var i=0; i < gestures.length; i++ ) {
+
+            var gesture = gestures[i];
+
+            if( gesture.state == 'start' ) {
+
+                console.log('Start circle');//, gesture);
+
+            } else if( gesture.state == 'update' ) {
+
+                //var clockwiseness = '';
+
+                console.log('Number of circles:', gesture.progress);
+
+                //if (gesture.pointable.direction.angleTo(.normal()) <= PI/4) { clockwiseness = "clockwise"; } else { clockwiseness = "counterclockwise"; }
+
+            } else if( gesture.state == 'stop' ) {
+
+                console.log('Stop circle');//, gesture);
+
+            }
+
+        }
+
+    }
+     */
 
 });
